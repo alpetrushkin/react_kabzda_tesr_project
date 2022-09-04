fb33bb8aa4ec57fe43b057c3162ad46bd4cdb5de
Александр <alpetrushkin1984@mail.ru>
2022-09-04 06:46:37 +0300

    one commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Accordion} from "./components/Accordion";
+import {Rating} from "./components/Rating";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   console.log("App rendering")
+   return (
+      <div className="App">
+         {/*<PageTitle title={"This is APP component"}/>*/}
+         {/*<PageTitle title={"My friends"}/>*/}
+         <Accordion title={"Menu"} collapsed={true} />
+         <Accordion title={"Users"} collapsed={false} />
+         {/*<Rating value={0}/>*/}
+         {/*<Rating value={1}/>*/}
+         {/*<Rating value={2}/>*/}
+         {/*<Rating value={3}/>*/}
+         {/*<Rating value={4}/>*/}
+         {/*<Rating value={5}/>*/}
+      </div>
+   );
+}
+
+interface PageTitlePropsType {
+   title: string
+}
+
+function PageTitle(props: PageTitlePropsType) {
+   console.log("PageTitle rendering")
+   return <div>{props.title}</div>
 }
 
 export default App;
